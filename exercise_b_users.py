9bc952b9f8a88a07e79d81972bae8f1f7e2fd6ef
Anita Safar <ancsa0416@gmail.com>
2021-10-12 20:51:42 +0100

    refreshing exercise b and c

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -55,12 +55,24 @@ users = {
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+users["Jonathan"]["twitter"]
 # 2. Get Erik's hometown
+users["Erik"]["home_town"]
 # 3. Get the array of Erik's lottery numbers
+users["Erik"]["lottery_numbers"]
 # 4. Get the species of Avril's pet Monty
+users["Avril"]["pets"]["species"]
 # 5. Get the smallest of Erik's lottery numbers
+smallest
+
+users["Erik"][]
 # 6. Return an array of Avril's lottery numbers that are even
+
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+users["Erik"]["lottery_numbers"].append(7)
 # 8. Change Erik's hometown to Edinburgh
+users["Erik"]["home_town"] = "Edinburgh"
 # 9. Add a pet dog to Erik called "Fluffy"
+users["Erik"]["pet dog"] = "Fluffy"
 # 10. Add another person to the users dictionary
+users["Lisa"] = {"twitter": "monalisa", "lottery_numbers": [1, 2, 3, 4, 5], "home_town": "Paris"}
